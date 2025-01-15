32b3b1c3c2c867e8ad8f978649937ed0f3d3f941
Mateie <contact@mateie.dev>
2025-01-15 13:25:59 -0800

    added a theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import "@fontsource/montserrat";
+
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,22 +1,35 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
+// Theme
+import { CssVarsProvider, extendTheme } from "@mui/joy/styles";
+
 // Dev tools
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+// Extended Theme
+const theme = extendTheme({
+    fontFamily: {
+        display: "Montserrat",
+        body: "Montserrat",
+    },
+});
 
 const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
     component: () => (
-        <QueryClientProvider client={queryClient}>
-            <Outlet />
-            {import.meta.env.DEV && (
-                <>
-                    <ReactQueryDevtools initialIsOpen={false} />
-                    <TanStackRouterDevtools />
-                </>
-            )}
-        </QueryClientProvider>
+        <CssVarsProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                <Outlet />
+                {import.meta.env.DEV && (
+                    <>
+                        <ReactQueryDevtools initialIsOpen={false} />
+                        <TanStackRouterDevtools />
+                    </>
+                )}
+            </QueryClientProvider>
+        </CssVarsProvider>
     ),
 });
