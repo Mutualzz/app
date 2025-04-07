c077b38d7a48b5f66278d6a7e14bc49123a52644
mateie <mateie@gmail.com>
2025-04-07 13:34:44 -0700

    added prettier and some eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import pluginRouter from "@tanstack/eslint-plugin-router";
 import pluginQuery from "@tanstack/eslint-plugin-query";
+import pluginRouter from "@tanstack/eslint-plugin-router";
+import pluginImport from "eslint-plugin-import";
+import pluginPrettier from "eslint-plugin-prettier/recommended";
+import pluginReact from "eslint-plugin-react";
+import pluginSonarjs from "eslint-plugin-sonarjs";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -12,11 +15,67 @@ export default [
     pluginJs.configs.recommended,
     ...tseslint.configs.recommended,
     pluginReact.configs.flat.recommended,
-    pluginRouter.configs.recommended,
-    pluginQuery.configs.recommended,
+    ...pluginRouter.configs["flat/recommended"],
+    ...pluginQuery.configs["flat/recommended"],
+    pluginSonarjs.configs.recommended,
+    pluginPrettier,
     {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+                project: "./tsconfig.json",
+            },
+        },
+        plugins: {
+            import: pluginImport,
+        },
         rules: {
+            "react/react-in-jsx-scope": "off",
             "react/no-unknown-property": ["error", { ignore: ["css"] }],
+            "no-console": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                {
+                    args: "all",
+                    vars: "all",
+                    argsIgnorePattern: "^_",
+                    caughtErrors: "all",
+                    caughtErrorsIgnorePattern: "^_",
+                    destructuredArrayIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    ignoreRestSiblings: true,
+                },
+            ],
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/consistent-type-imports": [
+                "error",
+                {
+                    prefer: "type-imports",
+                    fixStyle: "inline-type-imports",
+                },
+            ],
+            "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+            "@typescript-eslint/no-unnecessary-condition": "warn",
+            "@typescript-eslint/no-unnecessary-qualifier": "warn",
+            "@typescript-eslint/no-unnecessary-type-arguments": "warn",
+            "@typescript-eslint/no-unnecessary-type-constraint": "warn",
+            "@typescript-eslint/no-unnecessary-boolean-literal-compare": "warn",
+            "@typescript-eslint/no-misused-promises": "off",
+            "@typescript-eslint/restrict-template-expressions": "off",
+            "@typescript-eslint/prefer-for-of": "warn",
+            "@typescript-eslint/prefer-optional-chain": "warn",
+            "@typescript-eslint/no-unsafe-assignment": "warn",
+            "sonarjs/cognitive-complexity": "off",
+            "sonarjs/no-nested-conditional": "off",
+            "sonarjs/no-small-switch": "warn",
+            "sonarjs/no-nested-template-literals": "off",
+            "sonarjs/no-unused-vars": "warn",
+            "sonarjs/no-dead-store": "warn",
+            "sonarjs/unused-import": "warn",
+            "sonarjs/todo-tag": "warn",
+            "sonarjs/no-commented-code": "warn",
+            "import/no-cycle": "error",
         },
     },
 ];
