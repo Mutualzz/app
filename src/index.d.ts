ecc91321ed6d8097c78a341ec5f60f9e3bebe49b
Mateie <contact@mateie.dev>
2025-04-12 01:30:12 -0700

    removed css response value (not needed)

--- src/index.d.ts
+++ src/index.d.ts
@@ -7,18 +7,6 @@ declare module "@mutualzz/theme" {
 
     export type ColorLike = Hex | RGB | RGBA | HSL | HSLA;
 
-    export type CSSResponsiveValue =
-        | `${number}px`
-        | `${number}%`
-        | `${number}rem`
-        | `${number}em`
-        | `${number}vw`
-        | `${number}vh`
-        | `${number}vmin`
-        | `${number}vmax`
-        | `${number}fr`
-        | `${number}`; // plain numbers as strings like "12" // fallback
-
     export type ThemeColor =
         | "primary"
         | "neutral"
