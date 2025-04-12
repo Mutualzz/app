6783778d61198cfb3a8cc2be3479b3336cb49502
Mateie <contact@mateie.dev>
2025-04-11 22:56:56 -0700

    new utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,42 @@
+import type { ColorLike, Hex, ThemeColor } from "@mutualzz/theme";
 import type { PaperElevation } from "@ui/surfaces/Paper/Paper.types";
-import type { ColorInstance } from "color";
 import Color from "color";
-import type { HexString } from "./emotion";
 
 export const dynamicElevation = (
-    color: ColorInstance,
+    color: ColorLike,
     elevation: PaperElevation,
 ) => {
-    color = Color(color);
+    const instance = Color(color);
 
-    const baseLightness = color.lightness();
+    const baseLightness = instance.lightness();
     const increment = 2;
 
     const newLightness = Math.min(baseLightness + elevation * increment, 100);
 
-    return color.lightness(newLightness).hexa() as HexString;
+    return instance.lightness(newLightness).hexa() as Hex;
+};
+
+export const isThemeColor = (color: unknown): color is ThemeColor => {
+    return (
+        typeof color === "string" &&
+        ["primary", "neutral", "success", "error", "warning", "info"].includes(
+            color,
+        )
+    );
+};
+
+export const parseResponsiveValue = (value: string): string | number => {
+    if (value.trim() === "") return value;
+
+    const cssUnitRegex =
+        /^[+-]?(?:\d+\.\d+|\d+|\.\d+)(px|%|rem|em|vw|vh|vmin|vmax|fr)$/;
+    const keywordSet = new Set(["sm", "md", "lg"]);
+
+    if (cssUnitRegex.test(value)) return value;
+    if (keywordSet.has(value)) return value;
+
+    const num = Number(value);
+    if (!isNaN(num)) return num;
+
+    return value;
 };
