4ce348393bff56afe725abf56814309742c10d26
Mateie <contact@mateie.dev>
2025-05-08 19:18:54 -0700

    finished up the playground

--- src/routes/ui/typography.lazy.tsx
+++ src/routes/ui/typography.lazy.tsx
@@ -1,10 +1,19 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { Typography } from "@ui/components/data-display/Typography/Typography";
 import { type TypographyVariant } from "@ui/components/data-display/Typography/Typography.types";
 import { Stack } from "@ui/components/layout/Stack/Stack";
 import { Paper } from "@ui/components/surfaces/Paper/Paper";
-import type { Color } from "@ui/types";
-import { chunk } from "lodash";
-import { Typography } from "../../ui/src/components/data-display/Typography/Typography";
+import {
+    Button,
+    Divider,
+    RadioButton,
+    RadioButtonGroup,
+    randomHexColor,
+    useColorInput,
+} from "@ui/index";
+import type { Color, ColorLike, TypographyLevel } from "@ui/types";
+import { capitalize } from "lodash";
+import { useState } from "react";
 
 export const Route = createLazyFileRoute("/ui/typography")({
     component: PlaygroundTypography,
@@ -17,6 +26,27 @@ const variants = [
     "soft",
     "none",
 ] as TypographyVariant[];
+
+const levels = [
+    "display-lg",
+    "display-md",
+    "display-sm",
+    "display-xs",
+    "h1",
+    "h2",
+    "h3",
+    "h4",
+    "h5",
+    "h6",
+    "title-lg",
+    "title-md",
+    "title-sm",
+    "body-lg",
+    "body-md",
+    "body-sm",
+    "body-xs",
+] as TypographyLevel[];
+
 const colors = [
     "primary",
     "neutral",
@@ -27,41 +57,217 @@ const colors = [
 ] as Color[];
 
 function PlaygroundTypography() {
-    let typographies = [];
+    const [variant, setVariant] = useState<TypographyVariant>("solid");
+    const [level, setLevel] = useState<TypographyLevel>("body-md");
+    const [text, setText] = useState<string | null>(null);
 
-    for (const variant of variants) {
-        for (const color of [...colors]) {
-            typographies.push(
-                <Paper
-                    key={`${color}-${variant}`}
-                    elevation={4}
-                    p={20}
-                    spacing={10}
-                >
-                    <Typography level="body-md" color={color} variant={variant}>
-                        {`Typography ${color} ${variant}`}
-                    </Typography>
-                </Paper>,
-            );
-        }
-    }
+    const [customColors, setCustomColors] = useState<ColorLike[]>([]);
+    const [colorToDelete, setColorToDelete] = useState<ColorLike | null>(null);
 
-    typographies = chunk(typographies, [...colors].length).map((row, index) => (
-        <Stack key={index} p={20} spacing={10}>
-            {row}
-        </Stack>
+    const {
+        inputValue: inputColorValue,
+        color: customColor,
+        isInvalid,
+        handleChange,
+        validate,
+        setColorDirectly,
+    } = useColorInput<Color | ColorLike>();
+
+    const typographies = [...colors, ...customColors].map((color) => (
+        <Typography key={color} level={level} variant={variant} color={color}>
+            {text ?? `${capitalize(variant)} ${capitalize(color)}`}
+        </Typography>
     ));
 
     return (
         <Stack
             pt={40}
-            width="100%"
             spacing={20}
             direction="row"
-            justifyContent="center"
+            justifyContent="space-around"
         >
-            <Paper direction="column" alignItems="center" p={20} spacing={5}>
-                <Stack direction="column">{typographies}</Stack>
+            <Paper
+                direction="row"
+                alignItems="flex-start"
+                alignContent="flex-start"
+                wrap="wrap"
+                p={20}
+                spacing={5}
+                width={1200}
+            >
+                {variant === "none" && (
+                    <Typography level={level} variant={variant}>
+                        {text ?? "No variant applied"}
+                    </Typography>
+                )}
+                {variant !== "none" && typographies}
+            </Paper>
+            <Paper width={300} alignItems="center" direction="column" p={20}>
+                <Divider>Playground</Divider>
+                <Stack width="100%" direction="column" spacing={40}>
+                    <Stack direction="column" spacing={10}>
+                        <label>Variant</label>
+                        <RadioButtonGroup
+                            onChange={(_, vriant) =>
+                                setVariant(vriant as TypographyVariant)
+                            }
+                            value={variant}
+                            name="variant"
+                        >
+                            {variants.map((v) => (
+                                <RadioButton
+                                    key={v}
+                                    value={v}
+                                    label={capitalize(v)}
+                                    checked={variant === v}
+                                    color="neutral"
+                                    onChange={() => setVariant(v)}
+                                />
+                            ))}
+                        </RadioButtonGroup>
+                    </Stack>
+                    <Stack direction="column" spacing={10}>
+                        <label>Level</label>
+                        <select
+                            value={level}
+                            onChange={(e) =>
+                                setLevel(e.target.value as TypographyLevel)
+                            }
+                            style={{
+                                width: "100%",
+                                padding: 10,
+                                borderRadius: 5,
+                                border: "1px solid #ccc",
+                                backgroundColor: "#f9f9f9",
+                            }}
+                        >
+                            {levels.map((l) => (
+                                <option key={l} value={l}>
+                                    {l}
+                                </option>
+                            ))}
+                        </select>
+                    </Stack>
+                    {variant !== "none" && (
+                        <Stack direction="column" spacing={10}>
+                            <label>Custom Color</label>
+                            <Stack
+                                alignContent="center"
+                                direction="row"
+                                spacing={5}
+                            >
+                                <input
+                                    type="text"
+                                    value={inputColorValue}
+                                    placeholder="Input custom color"
+                                    onChange={(e) =>
+                                        handleChange(e.target.value)
+                                    }
+                                    onBlur={validate}
+                                    style={{
+                                        padding: 10,
+                                        borderRadius: 5,
+                                        border: isInvalid
+                                            ? "1px solid red"
+                                            : "1px solid #ccc",
+                                        backgroundColor: "#f9f9f9",
+                                        width: "100%",
+                                    }}
+                                />
+                                <Button
+                                    color="primary"
+                                    disabled={!customColor}
+                                    onClick={() => {
+                                        setCustomColors(
+                                            (prev) =>
+                                                [
+                                                    ...prev,
+                                                    customColor,
+                                                ] as ColorLike[],
+                                        );
+                                        setColorDirectly(randomHexColor());
+                                        setColorToDelete(
+                                            customColor as ColorLike,
+                                        );
+                                    }}
+                                >
+                                    Add Color
+                                </Button>
+                            </Stack>
+                            {customColors.length > 0 && (
+                                <Stack
+                                    alignItems="center"
+                                    direction="row"
+                                    spacing={10}
+                                >
+                                    <select
+                                        value={colorToDelete ?? ""}
+                                        onChange={(e) => {
+                                            setColorToDelete(
+                                                e.target.value.trim() as ColorLike,
+                                            );
+                                        }}
+                                        style={{
+                                            padding: 10,
+                                            borderRadius: 5,
+                                            border: "1px solid #ccc",
+                                            backgroundColor: "#f9f9f9",
+                                            width: "100%",
+                                        }}
+                                    >
+                                        {customColors.map((color) => (
+                                            <option key={color} value={color}>
+                                                {color}
+                                            </option>
+                                        ))}
+                                    </select>
+                                    <Button
+                                        color="danger"
+                                        onClick={() => {
+                                            setCustomColors((prev) => {
+                                                const updated = prev.filter(
+                                                    (color) =>
+                                                        color !== colorToDelete,
+                                                );
+                                                setColorToDelete(
+                                                    updated.length > 0
+                                                        ? updated[
+                                                              updated.length - 1
+                                                          ]
+                                                        : null,
+                                                );
+                                                return updated;
+                                            });
+                                        }}
+                                    >
+                                        Delete Color
+                                    </Button>
+                                </Stack>
+                            )}
+                        </Stack>
+                    )}
+                    <Stack direction="column" spacing={10}>
+                        <label>Text</label>
+                        <input
+                            type="text"
+                            value={text ?? ""}
+                            onChange={(e) =>
+                                e.target.value.trim() === ""
+                                    ? setText(null)
+                                    : setText(e.target.value)
+                            }
+                            style={{
+                                padding: 10,
+                                borderRadius: 5,
+                                border: isInvalid
+                                    ? "1px solid red"
+                                    : "1px solid #ccc",
+                                backgroundColor: "#f9f9f9",
+                                width: "100%",
+                            }}
+                        />
+                    </Stack>
+                </Stack>
             </Paper>
         </Stack>
     );
