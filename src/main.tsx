00b6b19959a3aa4351fc2a401906fb34a91382c3
Mateie <contact@mateie.dev>
2025-05-14 23:35:33 -0700

    fixed font imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import "@fontsource/inter";
-import "@fontsource/manrope";
+import "@fontsource/inter/100.css";
+import "@fontsource/inter/200.css";
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/inter/900.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
