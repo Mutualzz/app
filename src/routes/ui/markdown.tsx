5a5886a9287b13a34f80f8d0c2107d653fda9df1
Azrael <azrael@mutualzz.com>
2025-06-29 21:23:31 -0700

    added variants and colors

--- src/routes/ui/markdown.tsx
+++ src/routes/ui/markdown.tsx
@@ -1,6 +1,23 @@
 import { Markdown } from "@components/Markdown/Markdown";
 import { createFileRoute } from "@tanstack/react-router";
-import { Paper, Stack, Typography } from "@ui/index";
+import { Input } from "@ui/components/inputs/Input/Input";
+import {
+    Button,
+    Checkbox,
+    Divider,
+    Paper,
+    Radio,
+    RadioGroup,
+    randomHexColor,
+    Stack,
+    Typography,
+    useColorInput,
+    type Color,
+    type ColorLike,
+    type Variant,
+} from "@ui/index";
+import capitalize from "lodash-es/capitalize";
+import { useState } from "react";
 import { seo } from "seo";
 
 export const Route = createFileRoute("/ui/markdown")({
@@ -14,16 +31,132 @@ export const Route = createFileRoute("/ui/markdown")({
     }),
 });
 
+const variants = ["solid", "outlined", "plain", "soft"] as Variant[];
+const colors = [
+    "primary",
+    "neutral",
+    "success",
+    "danger",
+    "warning",
+    "info",
+] as Color[];
+
 function RouteComponent() {
+    const [variant, setVariant] = useState<Variant>("outlined");
+    const [color, setColor] = useState<Color>("primary");
+
+    const [customColorEnabled, setCustomColorEnabled] = useState(false);
+
+    const {
+        inputValue: inputColorValue,
+        color: customColor,
+        isInvalid,
+        handleChange,
+        setColorDirectly,
+        validate,
+    } = useColorInput<Color | ColorLike>();
+
     return (
-        <Paper direction="column" pt={8} width="100%" gap={2}>
-            <Typography color="danger" level="display-xs" textAlign="center">
-                This markdown editor is experimental and not fully functional
-                yet.
-            </Typography>
-            <Stack height="100%" p={12}>
-                <Markdown />
-            </Stack>
-        </Paper>
+        <Stack direction="row" spacing={10} width="100%" gap={2}>
+            <Paper width="100%" direction="column" pt={4}>
+                <Typography color="danger" level="title-lg" textAlign="center">
+                    This markdown editor is experimental and not fully
+                    functional yet.
+                </Typography>
+                <Stack height="100%" p={12}>
+                    <Markdown
+                        color={customColorEnabled ? customColor : color}
+                        variant={variant}
+                    />
+                </Stack>
+            </Paper>
+            <Paper width={300} direction="column" p={20}>
+                <Divider>Playground</Divider>
+                <Stack width="100%" direction="column" spacing={5}>
+                    <Stack direction="column" spacing={5}>
+                        <label>Variant</label>
+                        <RadioGroup
+                            onChange={(_, vriant) =>
+                                setVariant(vriant as Variant)
+                            }
+                            value={variant}
+                            name="variants"
+                        >
+                            {variants.map((v) => (
+                                <Radio
+                                    key={v}
+                                    value={v}
+                                    label={capitalize(v)}
+                                    checked={variant === v}
+                                    color="neutral"
+                                    onChange={() => setVariant(v)}
+                                />
+                            ))}
+                        </RadioGroup>
+                    </Stack>
+                    <Stack direction="column" spacing={5}>
+                        <Stack
+                            direction="row"
+                            justifyContent="space-between"
+                            spacing={5}
+                        >
+                            <label>Color</label>
+                            <Checkbox
+                                label="Custom"
+                                checked={customColorEnabled}
+                                onChange={(e) =>
+                                    setCustomColorEnabled(e.target.checked)
+                                }
+                            />
+                        </Stack>
+                        {customColorEnabled ? (
+                            <Stack direction="row" spacing={5}>
+                                <Input
+                                    variant="solid"
+                                    size="lg"
+                                    color="primary"
+                                    fullWidth
+                                    error={isInvalid}
+                                    placeholder="Enter a color (e.g. #ff0000)"
+                                    value={inputColorValue}
+                                    onChange={(e) => {
+                                        handleChange(e.target.value);
+                                    }}
+                                    onBlur={validate}
+                                />
+                                <Button
+                                    variant="solid"
+                                    color="neutral"
+                                    onClick={() => {
+                                        setColorDirectly(randomHexColor());
+                                    }}
+                                >
+                                    Random
+                                </Button>
+                            </Stack>
+                        ) : (
+                            <RadioGroup
+                                onChange={(_, color) =>
+                                    setColor(color as Color)
+                                }
+                                value={color}
+                                name="colors"
+                            >
+                                {colors.map((c) => (
+                                    <Radio
+                                        key={c}
+                                        value={c}
+                                        label={capitalize(c)}
+                                        checked={color === c}
+                                        color="neutral"
+                                        onChange={() => setColor(c)}
+                                    />
+                                ))}
+                            </RadioGroup>
+                        )}
+                    </Stack>
+                </Stack>
+            </Paper>
+        </Stack>
     );
 }
