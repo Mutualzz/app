bd8ee04b57039fb3cb88b4d1138392931e9f8e3f
Azrael <azrael@mutualzz.com>
2025-07-17 23:40:07 -0700

    fixed eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,13 @@
 import tanstack from "@mutualzz/eslint-config/tanstack";
 
-export default tanstack;
+export default [
+    ...tanstack,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
+    },
+];
