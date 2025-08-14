3f24ca13ab5915148183bb577db8d163134fe34a
mateie <mateie@gmail.com>
2025-08-13 20:23:54 -0700

    removed contextualized queryClient and appStore (not needed)

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 
-import { useAppStore } from "@hooks/useAppStore";
 import { routeTree } from "./routeTree.gen";
 
 export function createRouter() {
     const queryClient = new QueryClient();
-    const app = useAppStore();
 
     return createTanStackRouter({
         routeTree,
@@ -14,7 +12,6 @@ export function createRouter() {
         defaultPreloadStaleTime: 0,
         scrollRestoration: true,
         defaultSsr: false,
-        context: { queryClient, app },
 
         // eslint-disable-next-line react/prop-types
         Wrap: ({ children }) => (
