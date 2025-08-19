65928e65da49c50bb865f0bf87e1efc0c9732f31
mateie <mateie@gmail.com>
2025-08-18 18:44:55 -0700

    AccountStore: Implemented avatar related properties

--- src/stores/Account.store.ts
+++ src/stores/Account.store.ts
@@ -1,12 +1,23 @@
-import type { APITheme, APIUser, APIUserSettings } from "@mutualzz/types";
+import {
+    CDNRoutes,
+    ImageFormat,
+    type APITheme,
+    type APIUser,
+    type APIUserSettings,
+    type DefaultAvatar,
+} from "@mutualzz/types";
+import REST from "@utils/REST";
 import { makeAutoObservable } from "mobx";
 
 export class AccountStore {
     id: string;
     username: string;
+    defaultAvatar: DefaultAvatar;
+    previousAvatars: string[] = [];
+    avatar?: string | null = null;
     globalName?: string | null = null;
     email?: string | null = null;
-    themes?: APITheme[] | null = null;
+    themes: APITheme[] | null = [];
     createdAt: Date;
     createdTimestamp: number;
     settings: APIUserSettings;
@@ -16,9 +27,12 @@ export class AccountStore {
     constructor(user: APIUser) {
         this.id = user.id;
         this.username = user.username;
+        this.defaultAvatar = user.defaultAvatar;
+        this.avatar = user.avatar ?? null;
+        this.previousAvatars = user.previousAvatars ?? [];
         this.globalName = user.globalName ?? null;
         this.email = user.email ?? null;
-        this.themes = user.themes ?? null;
+        this.themes = user.themes ?? [];
         this.settings = user.settings;
         this.createdAt = user.createdAt;
         this.createdTimestamp = user.createdTimestamp;
@@ -26,5 +40,13 @@ export class AccountStore {
         this.raw = user;
 
         makeAutoObservable(this);
+    }
+
+    get avatarUrl() {
+        return REST.makeCDNUrl(
+            this.avatar
+                ? CDNRoutes.userAvatar(this.id, this.avatar, ImageFormat.PNG)
+                : CDNRoutes.defaultUserAvatar(this.defaultAvatar),
+        );
     }
 }
