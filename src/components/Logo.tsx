2b220adce2f698a89a1a854ea58995292ab30e62
mateie <mateie@gmail.com>
2025-08-23 20:12:40 -0700

    Logo: Swapped the names for light and dark logo ^^

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ export const Logo = (props: HTMLAttributes<HTMLImageElement>) => {
 
     return (
         <img
-            src={theme.type === "light" ? "/logo-light.png" : "/logo.png"}
+            src={theme.type === "light" ? "/logo.png" : "/logo-dark.png"}
             alt="Mutualzz Logo"
             {...props}
         />
