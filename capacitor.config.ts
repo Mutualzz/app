ada284713ffa85139513c7d7f915a64b76512fa7
mateie <mateie@gmail.com>
2025-08-23 20:25:12 -0700

    Tauri & Capacitor: Changed app identifier (If old versions are installed, you have to uninstall the old one manually, and install the new one. Im sorry :<)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,16 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-    appId: "com.mutualzz.mz",
+    appId: "com.mutualzz.app",
     appName: "Mutualzz",
     webDir: "dist",
     server: { cleartext: true, url: "http://137.150.244.173:1420" },
     plugins: {
         StatusBar: {
             overlaysWebView: false,
+        },
+        CapacitorHttp: {
+            enabled: true,
         },
     },
 };
