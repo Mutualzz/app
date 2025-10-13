26588311ab9be950171ecc124202a9f739b913e4
Azrael <azraelmachitidze@gmail.com>
2025-10-13 13:38:00 -0700

    fix again

--- src/components/CropperClient.tsx
+++ src/components/CropperClient.tsx
@@ -1,10 +1,11 @@
+import { isSSR } from "@utils/index";
 import { lazy, Suspense } from "react";
 
 // Dynamically import only in the browser
 const LazyCropper = lazy(() => import("react-easy-crop"));
 
 export function CropperClient(props: any) {
-    if (import.meta.env.SSR) return null;
+    if (isSSR) return null;
     return (
         <Suspense fallback={null}>
             <LazyCropper {...props} />
