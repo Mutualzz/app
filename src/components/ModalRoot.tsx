e607dc0b47c3e470c0dc877aef5d284dc890b3b8
Azrael <azraelmachitidze@gmail.com>
2025-10-13 23:47:34 -0700

    Modals: Removed padding top for safe areas (not needed)

--- src/components/ModalRoot.tsx
+++ src/components/ModalRoot.tsx
@@ -1,13 +1,9 @@
 import { useModal } from "@contexts/Modal.context";
 import { Modal, useTheme } from "@mutualzz/ui-web";
-import { useMediaQuery } from "@react-hookz/web";
 
 export const ModalRoot = () => {
     const { theme } = useTheme();
     const { modals, closeModal } = useModal();
-    const isMobileQuery = useMediaQuery(
-        theme.breakpoints.down("md").replace("@media ", ""),
-    );
 
     if (modals.length === 0) return null;
 
@@ -22,11 +18,6 @@ export const ModalRoot = () => {
                     css={{
                         zIndex: theme.zIndex.modal + idx,
                     }}
-                    pt={
-                        isMobileQuery
-                            ? "max(2rem, env(safe-area-inset-bottom, 0px))"
-                            : undefined
-                    }
                 >
                     {modal.content}
                 </Modal>
