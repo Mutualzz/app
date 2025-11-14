3b3688df7a328f8a1631b06b436616fb97eae907
Azrael <azrael@mutualzz.com>
2025-11-13 16:14:41 -0800

    fixing ssr

--- vite.config.ts
+++ vite.config.ts
@@ -92,6 +92,10 @@ export default defineConfig({
         }),
     ],
 
+    ssr: {
+        noExternal: ["react-easy-crop", "tslib"],
+    },
+
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     //
     // 1. prevent vite from obscuring rust errors
