e727232d7fcf78c6721ee76d781f3f76619d64e1
Azrael <azrael@mutualzz.com>
2026-05-18 00:30:54 -0700

    Trynna fix bugs for electron

--- src/renderer/src/routes/_authenticated/route.tsx
+++ src/renderer/src/routes/_authenticated/route.tsx
@@ -3,11 +3,13 @@ import { createFileRoute, Navigate, Outlet } from "@tanstack/react-router";
 import { observer } from "mobx-react-lite";
 
 export const Route = createFileRoute("/_authenticated")({
-    component: observer(AuthenticatedRoute),
+    component: observer(AuthenticatedRoute)
 });
 
 function AuthenticatedRoute() {
     const app = useAppStore();
+
+    if (!app.settings) return null;
 
     if (!app.token) return <Navigate to="/login" replace />;
 
--- src/renderer/src/routes/index.tsx
+++ src/renderer/src/routes/index.tsx
@@ -4,7 +4,7 @@ import { observer } from "mobx-react-lite";
 import { useEffect } from "react";
 
 export const Route = createFileRoute("/")({
-    component: observer(RouteComponent),
+    component: observer(RouteComponent)
 });
 
 function RouteComponent() {
@@ -12,33 +12,34 @@ function RouteComponent() {
 
     useEffect(() => {
         app.resetMode();
-    }, []);
+    }, [app]);
 
-    if (app.joiningSpace && app.joiningInviteCode)
+    if (!app.settings) return null;
+
+    if (app.joiningSpace && app.joiningInviteCode) {
         return (
             <Navigate
                 to="/invite/$code"
                 replace
                 params={{
-                    code: app.joiningInviteCode,
+                    code: app.joiningInviteCode
                 }}
                 search={{
-                    deepLink: false,
+                    deepLink: false
                 }}
             />
         );
+    }
 
     if (!app.token) return <Navigate to="/login" replace />;
-
-    if (!app.settings) return null;
 
     const lastHref = app.navigation.current?.href;
     if (
         lastHref &&
         (lastHref.startsWith("/spaces") || lastHref.startsWith("/feed"))
-    )
+    ) {
         return <Navigate to={lastHref} replace />;
+    }
 
-    // NOTE: This "as" is added because we haven't implemented DMs yet
     return <Navigate to={`/${app.targetMode as "feed" | "spaces"}`} replace />;
 }
