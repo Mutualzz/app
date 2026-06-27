a4cba682ccf6d08a9727dbcabde2e5e74890e7ca
Azrael <azrael@mutualzz.com>
2026-06-26 19:45:44 -0700

    Profile: Fixed text block visuals

--- src/renderer/src/components/Profile/blocks/ProfileTextBlockView.tsx
+++ src/renderer/src/components/Profile/blocks/ProfileTextBlockView.tsx
@@ -2,28 +2,36 @@ import { Paper } from "@components/Paper";
 import { ProfileMarkdownContent } from "@components/Profile/shared/ProfileMarkdownContent";
 import type { ProfileTextBlock } from "@mutualzz/types";
 import { Box, Typography } from "@mutualzz/ui-web";
+import { useAppStore } from "@renderer/hooks/useStores";
 
 export const ProfileTextBlockView = ({
   block
 }: {
   block: ProfileTextBlock;
-}) => (
+}) => {
+  const app = useAppStore();
+
+  return (
     <Paper
       width="100%"
       height="100%"
-      variant="soft"
       borderRadius={8}
       p={1.5}
       css={{ overflow: "hidden" }}
+      elevation={app.settings?.preferEmbossed ? 5 : 1}
     >
       {block.content ? (
         <Box css={{ fontSize: "var(--pcf-md)" }}>
           <ProfileMarkdownContent value={block.content} />
         </Box>
       ) : (
-        <Typography level="body-md" css={{ opacity: 0.5, fontSize: "var(--pcf-md)" }}>
+        <Typography
+          level="body-md"
+          css={{ opacity: 0.5, fontSize: "var(--pcf-md)" }}
+        >
           Text
         </Typography>
       )}
     </Paper>
-);
+  );
+};
